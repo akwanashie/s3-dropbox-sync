852220a5370a43a404f850992025bb83fa30b028
Augustine Kwanashie <akwanashie@gmail.com>
2021-02-07 22:29:00 +0000

    Try out listing and writing to dropbox

--- src/publish.js
+++ src/publish.js
@@ -1,7 +1,26 @@
 const { Dropbox } = require('dropbox');
+const { readFileSync } = require('fs');
 
-const dropboxClient = new Dropbox({ accessToken: process.env.TOKEN });
+const dropboxClient = new Dropbox({ accessToken: process.env.DBX_TOKEN });
+const dropboxFolderPath = '/foreign-folders/s3'
+async function viewS3Folder() {
+  const folderList = (await dropboxClient.filesListFolder({ path: dropboxFolderPath })).result.entries;
+  return folderList.map(fileInfo => fileInfo.name);
+}
 
-dropboxClient.filesListFolder({ path: '' })
-  .then(console.log)
-  .catch(console.log);
+async function saveFileToS3Folder(content, fileName) {
+  const data = await dropboxClient.filesUpload({
+    contents: content,
+    path: `${dropboxFolderPath}/${fileName}`,
+    mode: { '.tag': 'overwrite' }
+  });
+ return data.result.path_lower;
+}
+
+// viewS3Folder()
+//   .then(console.log)
+//   .catch(console.log);
+
+// saveFileToS3Folder('This is a test 123', 'test.txt')
+//   .then(console.log)
+//   .catch(console.log);
